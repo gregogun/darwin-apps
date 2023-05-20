162e89808c335592a3f6ce829d2c793bb115d11d
gregogun <gregogun97@gmail.com>
2023-05-20 23:00:09 +0100

    fix: bundlr type

--- packages/cli/src/modules/createManifest/recursiveUpload.ts
+++ packages/cli/src/modules/createManifest/recursiveUpload.ts
@@ -1,13 +1,9 @@
-import Bundlr from "@bundlr-network/client/build/common/bundlr";
-import fs, { readFileSync, readdirSync } from "fs";
-import path from "path";
-import mime from "mime-types";
+import fs, { readFileSync, readdirSync } from 'fs';
+import path from 'path';
+import mime from 'mime-types';
+import NodeBundlr from '@bundlr-network/client/build/cjs/node/bundlr';
 
-export const recursiveUploadFiles = async (
-  dir: string,
-  paths: {},
-  bundlr: Bundlr
-) => {
+export const recursiveUploadFiles = async (dir: string, paths: {}, bundlr: NodeBundlr) => {
   const files = readdirSync(dir);
 
   // loop through files and upload
@@ -17,11 +13,11 @@ export const recursiveUploadFiles = async (
     if (fs.statSync(filepath).isDirectory()) {
       await recursiveUploadFiles(filepath, paths, bundlr);
     } else {
-      const contentType = mime.lookup(file) || "text/plain";
+      const contentType = mime.lookup(file) || 'text/plain';
       try {
-        const data = readFileSync(filepath, "utf-8");
+        const data = readFileSync(filepath, 'utf-8');
         const tx = await bundlr.createTransaction(data, {
-          tags: [{ name: "Content-Type", value: contentType }],
+          tags: [{ name: 'Content-Type', value: contentType }],
         });
         await tx.sign();
         await tx.upload();
